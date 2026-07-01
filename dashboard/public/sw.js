2d1248e272c886af5588764adcb037c1f28be520
Sergii Bondarenko <2760616+BR0kEN-@users.noreply.github.com>
2026-07-01 16:28:30 +0300

    upd (#66)

--- dashboard/public/sw.js
+++ dashboard/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'solaroid-shell-v12'
+const CACHE_NAME = 'solaroid-shell-v13'
 const SHELL_URLS = ['./', './manifest.webmanifest', './icon.svg', './icon-maskable.svg']
 
 self.addEventListener('install', (event) => {
